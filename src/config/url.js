cf5b5ccd154074ff404e2432874be3706aca5fe3
autsakorn <autsakorn@Autsakorns-MacBook-Pro.local>
2017-04-19 16:37:36 +0700

    Commit daily

--- src/config/url.js
+++ src/config/url.js
@@ -32,7 +32,8 @@ var Url = {
   'changeEndUserJson':END_POINT_2+"v1/enduser/ChangeEndUserJson",
   'changeAppointment':END_POINT_2+"v1/servicereport/EditTimeAppointmentMobile1",
   'update_spare_part':END_POINT_2+"v1/ticket/update_spare_part",
-  'input_action':END_POINT_2+"v1/insert/input_action"
+  'input_action':END_POINT_2+"v1/insert/input_action",
+  'delete_spare_part': END_POINT_2+"v1/ticket/deleteSparePart"
 }
 
 export default Url;
