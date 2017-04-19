f862a8e83c4c0facbfd504db3d2dd4285d71c458
autsakorn <autsakorn@Autsakorns-MacBook-Pro.local>
2017-04-19 14:00:44 +0700

    iamarm

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Im Autsakorn
 I'm Teerawut
 Im Mas
+Im arm
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/config/url.js
+++ src/config/url.js
@@ -30,7 +30,8 @@ var Url = {
   'appointment':END_POINT_2+"v1/servicereport/appointment",
   'checkpoint':END_POINT_2+"v1/timestampSr",
   'changeEndUserJson':END_POINT_2+"v1/enduser/ChangeEndUserJson",
-  'changeAppointment':END_POINT_2+"v1/servicereport/EditTimeAppointmentMobile1"
+  'changeAppointment':END_POINT_2+"v1/servicereport/EditTimeAppointmentMobile1",
+  'update_spare_part':END_POINT_2:"v1/ticket/update_spare_part"
 }
 
 export default Url;
